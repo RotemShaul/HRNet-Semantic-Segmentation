c88510fd117666332d405128e5eea505c6788abd
rotems <rotems@weizmann.ac.il>
2020-04-16 21:51:59 +0300

    adding proper sh

---
+++ 2_train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 /opt/conda/bin/python -m torch.distributed.launch --nproc_per_node=4 /home/labs/waic/rotems/code/HRNet-Semantic-Segmentation/tools/train.py --cfg /home/labs/waic/rotems/code/HRNet-Semantic-Segmentation/experiments/cityscapes/seg_hrnet_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484.yaml
