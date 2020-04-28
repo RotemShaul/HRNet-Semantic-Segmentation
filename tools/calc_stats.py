731c008863e020df09534c589b7f0f8ac7e10a4e
rotems <rotemshaul@gmail.com>
2020-04-28 22:07:55 +0300

    adding funcs to calc mean/std

---
+++ tools/calc_stats.py
@@ -0,0 +1,141 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Ke Sun (sunk@mail.ustc.edu.cn)
+# ------------------------------------------------------------------------------
+
+import argparse
+import os
+import pprint
+import shutil
+import sys
+
+import logging
+import time
+import timeit
+from pathlib import Path
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim
+from torch.utils.data.distributed import DistributedSampler
+from tensorboardX import SummaryWriter
+
+import _init_paths
+import models
+import datasets
+from config import config
+from config import update_config
+from core.criterion import CrossEntropy, OhemCrossEntropy
+from core.function import train, validate
+from utils.modelsummary import get_model_summary
+from utils.utils import create_logger, FullModel, get_rank
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train segmentation network')
+    
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    update_config(config, args)
+
+    return args
+
+def main():
+    args = parse_args()
+
+    logger, final_output_dir, tb_log_dir = create_logger(
+        config, args.cfg, 'train')
+
+    logger.info(pprint.pformat(args))
+    logger.info(config)
+
+    writer_dict = {
+        'writer': SummaryWriter(tb_log_dir),
+        'train_global_steps': 0,
+        'valid_global_steps': 0,
+    }
+
+    # cudnn related setting
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    cudnn.enabled = config.CUDNN.ENABLED
+    gpus = list(config.GPUS)
+    distributed = len(gpus) > 1
+    device = torch.device('cuda:{}'.format(args.local_rank))
+
+    # build model
+    model = eval('models.'+config.MODEL.NAME +
+                 '.get_seg_model')(config)
+
+    if args.local_rank == 0:
+        # provide the summary of model
+        dump_input = torch.rand(
+            (1, 3, config.TRAIN.IMAGE_SIZE[1], config.TRAIN.IMAGE_SIZE[0])
+            )
+
+        dummy_disp = torch.rand((1, config.TRAIN.IMAGE_SIZE[1], config.TRAIN.IMAGE_SIZE[0]))
+        logger.info(get_model_summary(model.to(device), dump_input.to(device), dummy_disp.to(device)))
+
+        # copy model file
+        this_dir = os.path.dirname(__file__)
+        models_dst_dir = os.path.join(final_output_dir, 'models')
+        if os.path.exists(models_dst_dir):
+            shutil.rmtree(models_dst_dir)
+        shutil.copytree(os.path.join(this_dir, '../lib/models'), models_dst_dir)
+    #
+    if distributed:
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(
+            backend="nccl", init_method="env://"
+        )
+
+    # prepare data
+    crop_size = (config.TRAIN.IMAGE_SIZE[1], config.TRAIN.IMAGE_SIZE[0])
+    train_dataset = eval('datasets.'+config.DATASET.DATASET)(
+                        root=config.DATASET.ROOT,
+                        list_path=config.DATASET.TRAIN_SET,
+                        num_samples=None,
+                        num_classes=config.DATASET.NUM_CLASSES,
+                        multi_scale=config.TRAIN.MULTI_SCALE,
+                        flip=config.TRAIN.FLIP,
+                        ignore_label=config.TRAIN.IGNORE_LABEL,
+                        base_size=config.TRAIN.BASE_SIZE,
+                        crop_size=crop_size,
+                        downsample_rate=config.TRAIN.DOWNSAMPLERATE,
+                        scale_factor=config.TRAIN.SCALE_FACTOR)
+
+    if distributed:
+        train_sampler = DistributedSampler(train_dataset)
+    else:
+        train_sampler = None
+
+    trainloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=config.TRAIN.BATCH_SIZE_PER_GPU,
+        shuffle=config.TRAIN.SHUFFLE and train_sampler is None,
+        num_workers=config.WORKERS,
+        pin_memory=True,
+        drop_last=True,
+        sampler=train_sampler)
+
+    for i_iter, batch in enumerate(trainloader):
+        disparity = batch
+        disparity = disparity.to(device)
+        print("disparity {}".format())
+
+
+
+if __name__ == '__main__':
+    main()
