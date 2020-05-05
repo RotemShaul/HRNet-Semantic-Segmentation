1f3476f3fb6508eb3fd3dbbd6c64c9c1f3792283
rotems <rotemshaul@gmail.com>
2020-05-06 01:01:49 +0300

    adding 2c option

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -9,5 +9,7 @@ from __future__ import division
 from __future__ import print_function
 
 from .cityscapes import Cityscapes as cityscapes
+from .cityscapes2c import Cityscapes2c as cityscapes2c
 from .lip import LIP as lip
 from .pascal_ctx import PASCALContext as pascal_ctx
+
